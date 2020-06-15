0d50b68a5ce298e0c984f5a7fdffebee343834d7
Artem Rebryk <artemrebrik8099@gmail.com>
2020-06-15 23:24:32 +0300

    fix deployment

--- functions/index.ts
+++ functions/index.ts
@@ -1,6 +1,6 @@
 
 import * as functions from 'firebase-functions';
-import { app } from '../src/app';
+const { app } = require('./app/app');
 
 export const bot = functions.https.onRequest(async (req, res) => {
     return app(req, res)

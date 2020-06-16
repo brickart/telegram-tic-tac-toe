9a2177c3396b6ef0d28003038ef1ba9b335e6bc4
Artem Rebryk <artemrebrik8099@gmail.com>
2020-06-16 14:27:25 +0300

    add locale system

---
+++ src/i18n/index.ts
@@ -0,0 +1,11 @@
+import en from './locales/en.json';
+import ru from './locales/ru.json';
+import ua from './locales/ua.json';
+
+const data = {};
+
+function getDataByLocale(locale: string): any {
+	
+}
+
+export default ;
---
+++ src/i18n/locale.d.ts
@@ -0,0 +1,7 @@
+declare namespace ILocale {
+    export interface ILocaleData {
+
+    }
+}
+
+export { ILocale }
